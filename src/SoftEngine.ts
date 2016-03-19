d2e4655e64d456a56920a7a0a8eff31e5a99baf8
David Bimmler <bimmlerd@student.ethz.ch>
2016-03-19 15:01:33 +0100

    load model from json

--- src/SoftEngine.ts
+++ src/SoftEngine.ts
@@ -11,36 +11,36 @@ export module SoftEngine {
         private workingHeight: number;
         // corresponds to backbuffer.data
         private backbufferdata;
-        
+
         constructor(canvas: HTMLCanvasElement) {
             this.workingCanvas = canvas;
             this.workingWidth = canvas.width;
             this.workingHeight = canvas.height;
             this.workingContext = this.workingCanvas.getContext("2d");
         }
-        
+
         // this function is called to clear the backbuffer with a specific color
         public clear(): void {
             this.workingContext.clearRect(0, 0, this.workingWidth, this.workingHeight);
             this.backbuffer = this.workingContext.getImageData(0, 0, this.workingWidth, this.workingHeight);
         }
-        
+
         // once everything is ready, we flush the back buffer into the front buffer
         public present(): void {
             this.workingContext.putImageData(this.backbuffer, 0, 0);
         }
-        
+
         public putPixel(x: number, y: number, color: BABYLON.Color4): void {
             this.backbufferdata = this.backbuffer.data;
             var index: number = ((x >> 0) + (y >> 0) * this.workingWidth) * 4; // why the shifts?
-            
+
             // RGBA
             this.backbufferdata[index + 0] = color.r * 255;
             this.backbufferdata[index + 1] = color.g * 255;
             this.backbufferdata[index + 2] = color.b * 255;
             this.backbufferdata[index + 3] = color.a * 255;
         }
-        
+
         public project(coord: BABYLON.Vector3, transMat: BABYLON.Matrix): BABYLON.Vector2 {
             // let babylon do the transformation
             var point: BABYLON.Vector3 = BABYLON.Vector3.TransformCoordinates(coord, transMat);
@@ -51,15 +51,15 @@ export module SoftEngine {
             var y: number = -point.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
             return (new BABYLON.Vector2(x, y));
         }
-        
+
         public drawPoint(point: BABYLON.Vector2): void {
             // Clipping what's visible on screen
             if (point.x >= 0 && point.y >= 0 && point.x < this.workingWidth
-                                             && point.y < this.workingHeight) {
+                && point.y < this.workingHeight) {
                 this.putPixel(point.x, point.y, new BABYLON.Color4(1, 1, 0, 1));
             }
         }
-        
+
         public drawLine(point0: BABYLON.Vector2, point1: BABYLON.Vector2): void {
             // Bresenham algorithm
             var x0 = point0.x >> 0;
@@ -81,7 +81,7 @@ export module SoftEngine {
                 var e2 = 2 * err;
 
                 if (e2 > -dy) {
-                    err -= dy; 
+                    err -= dy;
                     x0 += sx;
                 }
 
@@ -91,12 +91,12 @@ export module SoftEngine {
                 }
             }
         }
-        
+
         public render(camera: Camera, meshes: Mesh[]): void {
             var viewMatrix = BABYLON.Matrix.LookAtLH(camera.Position, camera.Target, BABYLON.Vector3.Up());
             var projectionMatrix = BABYLON.Matrix.PerspectiveFovLH(0.78,
                 this.workingWidth / this.workingHeight, 0.01, 1.0);
-            
+
             // draw each mesh
             for (var index = 0; index < meshes.length; index++) {
                 var cMesh = meshes[index];
@@ -105,39 +105,96 @@ export module SoftEngine {
                     .multiply(BABYLON.Matrix.Translation(
                         cMesh.Position.x, cMesh.Position.y, cMesh.Position.z));
                 var transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
-                
-                // draw vertices of current mesh
-                /*for (var indexVertices = 0; indexVertices < cMesh.Vertices.length; indexVertices++) {
-                    var projectedPoint = this.project(cMesh.Vertices[indexVertices], transformMatrix);
-                    this.drawPoint(projectedPoint);
-                }*/
-                
+
                 // draw faces of current mesh
                 for (var indexFaces = 0; indexFaces < cMesh.Faces.length; indexFaces++) {
                     var cFace = cMesh.Faces[indexFaces];
-                    
+
                     var vertexA = cMesh.Vertices[cFace.A];
                     var vertexB = cMesh.Vertices[cFace.B];
                     var vertexC = cMesh.Vertices[cFace.C];
-                    
+
                     var pixelA = this.project(vertexA, transformMatrix);
                     var pixelB = this.project(vertexB, transformMatrix);
                     var pixelC = this.project(vertexC, transformMatrix);
-                    
+
                     this.drawLine(pixelA, pixelB);
                     this.drawLine(pixelB, pixelC);
                     this.drawLine(pixelC, pixelA);
                 }
             }
         }
+
+        public LoadJSONFileAsync(fileName: string, callback: (result: Mesh[]) => any): void {
+            var jsonObject = {};
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", fileName, true);
+            var context = this;
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    jsonObject = JSON.parse(xmlhttp.responseText);
+                    callback(context.CreateMeshesFromJSON(jsonObject))
+                }
+            };
+            xmlhttp.send(null);
+        }
+
+
+        private CreateMeshesFromJSON(jsonObject): Mesh[] {
+            var meshes: Mesh[] = [];
+            for (var meshIndex = 0; meshIndex < jsonObject.meshes.length; meshIndex++) {
+                var verticesArray: number[] = jsonObject.meshes[meshIndex].vertices;
+                var indicesArray: number[] = jsonObject.meshes[meshIndex].indices;
+
+                var uvCount: number = jsonObject.meshes[meshIndex].uvCount;
+                var verticesStep = 1;
+
+                switch (uvCount) {
+                    case 0:
+                        verticesStep = 6;
+                        break;
+                    case 1:
+                        verticesStep = 8;
+                        break;
+                    case 2:
+                        verticesStep = 10;
+                        break;
+                }
+
+                var verticesCount = verticesArray.length / verticesStep;
+                var facesCount = indicesArray.length / 3;
+                var mesh = new SoftEngine.Mesh(jsonObject.meshes[meshIndex].name, verticesCount, facesCount);
+
+                for (var index = 0; index < verticesCount; index++) {
+                    var x = verticesArray[index * verticesStep];
+                    var y = verticesArray[index * verticesStep + 1];
+                    var z = verticesArray[index * verticesStep + 2];
+
+                    mesh.Vertices[index] = new BABYLON.Vector3(x, y, z);
+                }
+
+                for (var index = 0; index < facesCount; index++) {
+                    var a = indicesArray[index * 3];
+                    var b = indicesArray[index * 3 + 1]
+                    var c = indicesArray[index * 3 + 2]
+                    mesh.Faces[index] = { A: a, B: b, C: c };
+                }
+
+                var position = jsonObject.meshes[meshIndex].position;
+                mesh.Position = new BABYLON.Vector3(position[0], position[1], position[2])
+
+                meshes.push(mesh);
+            }
+            return meshes;
+        }
     }
-    
+
     export class Mesh {
         Position: BABYLON.Vector3;
         Rotation: BABYLON.Vector3;
         Vertices: BABYLON.Vector3[];
         Faces: Face[];
-        
+
         constructor(public name: string, facesCount: number, verticesCount: number) {
             this.Vertices = new Array(verticesCount);
             this.Faces = new Array(facesCount);
@@ -145,17 +202,17 @@ export module SoftEngine {
             this.Position = BABYLON.Vector3.Zero();
         }
     }
-    
+
     export class Camera {
         Position: BABYLON.Vector3;
         Target: BABYLON.Vector3;
-        
+
         constructor() {
             this.Position = BABYLON.Vector3.Zero();
             this.Target = BABYLON.Vector3.Zero();
         }
     }
-    
+
     export interface Face {
         A: number;
         B: number;
